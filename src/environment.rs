58e02e058aac7c2e78f78bd9f69ff7860dec5487
Julian Pollinger <julian@pollinger.dev>
2025-01-16 14:38:00 +0100

    Add dotenv support and improve configuration handling
    Introduced dotenv for environment variable management, replacing hardcoded variables for better flexibility and security. Updated scripts to handle HTTP status codes on uploads and added a `.env.template` file for easier setup. Adjusted gitignore and bumped project version to 0.1.3.

--- src/environment.rs
+++ src/environment.rs
@@ -1,8 +1,16 @@
 use std::env;
-
+use std::io::ErrorKind;
 pub fn get_environment_variable(name: &str) -> String {
     match env::var(name) {
         Ok(url) => url,
         Err(_e) => panic!("Failed to read environment variable '{}'", name),
+    }
+}
+
+pub fn load_dotenv() -> dotenvy::Result<()> {
+    match dotenvy::dotenv() {
+        Ok(_) => Ok(()),
+        Err(dotenvy::Error::Io(err)) if (err.kind() == ErrorKind::NotFound) => Ok(()),
+        Err(e) => Err(e),
     }
 }
